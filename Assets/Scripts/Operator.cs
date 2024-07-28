5b90852d02ce7303623f2bf1478fd8c29646f7b8
terumasa <m24j4045a@edu.tottori-u.ac.jp>
2024-07-28 20:18:23 +0900

    Add: Add constructor to Operator.cs

--- Assets/Scripts/Operator.cs
+++ Assets/Scripts/Operator.cs
@@ -17,6 +17,19 @@ public class Operator {
     //持ち駒から置けるところがない場合のコンストラクタ
     public Operator(List<int> availablePositonsList, int sourcePos, int targetPos, int koma) {
         this.availablePositonsList = availablePositonsList;
+        this.sourcePos = sourcePos;//使われてないかも、AIが使う？
+        this.targetPos = targetPos;
+        this.koma = koma;
+    }
+
+    //持ち駒から置けるところがある場合のコンストラクタ
+    public Operator(int targetPos, int koma) {
+        this.targetPos = targetPos;
+        this.koma = koma;
+    }
+
+    //持ち駒から置けるところがない場合のコンストラクタ
+    public Operator(int sourcePos, int targetPos, int koma) {
         this.sourcePos = sourcePos;
         this.targetPos = targetPos;
         this.koma = koma;
